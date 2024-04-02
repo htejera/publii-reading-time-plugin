9e00a24a88df6d4dd4813719868c09501055948c
htejera <ohtejera@vivaldi.net>
2024-04-02 14:34:39 -0300

    Improve text customization and parent element selection
    Enhance internationalization and customization by introducing a placeholder pattern for dynamic reading time text and allowing users to specify a parent element selector for the reading time display.

--- main.js
+++ main.js
@@ -10,30 +10,37 @@ class ReadingTimePlugin {
     }
 
     injectReadingTime() {
-        let text = this.config.text || 'Estimated reading time:';
+        let text = this.config.text || 'Estimated reading time: %s minute(s) to read';
+        let parentElementSelector = this.config.parentElementSelector || 'h1';
+
         let scriptContent = `
         document.addEventListener("DOMContentLoaded", function() {
-            function calculateReadingTime() {
-                const textElements = document.querySelectorAll('.post__entry p');
-                let totalWords = 0;
-                textElements.forEach(element => {
-                    totalWords += element.innerText.split(' ').filter(Boolean).length;
-                });
-                const wordsPerMinute = 200;
-                const readingTimeDecimal = totalWords / wordsPerMinute;
-                const readingTimeMinutes = Math.floor(readingTimeDecimal);
-                const fractionalMinute = readingTimeDecimal - readingTimeMinutes;
-                const readingTimeSeconds = Math.round(fractionalMinute * 60);
-                const readingTimeElement = document.createElement('div');
-                readingTimeElement.className = 'reading-time';                
-                readingTimeElement.textContent = '${text}' + ' ' + readingTimeMinutes + '.' + readingTimeSeconds + ' minute(s) to read';
-                const postTitle = document.querySelector('h1');
-                if (postTitle) {
-                    postTitle.insertAdjacentElement('afterend', readingTimeElement);
-                }
-            }
-
             if (window.location.pathname !== '/') {
+                function calculateReadingTime() {
+                    const textElements = document.querySelectorAll('.post__entry p');
+                    let totalWords = 0;
+                    textElements.forEach(element => {
+                        totalWords += element.innerText.split(' ').filter(Boolean).length;
+                    });
+
+                    const wordsPerMinute = 200;
+                    const readingTimeDecimal = totalWords / wordsPerMinute;
+                    const readingTimeMinutes = Math.floor(readingTimeDecimal);
+                    const fractionalMinute = readingTimeDecimal - readingTimeMinutes;
+                    const readingTimeSeconds = Math.round(fractionalMinute * 60);
+                    const timeString = readingTimeMinutes + (readingTimeSeconds > 0 ? '.' + readingTimeSeconds : '');
+                    const finalText = '${text}'.replace('%s', timeString);
+
+                    const readingTimeElement = document.createElement('div');
+                    readingTimeElement.className = 'reading-time';                
+                    readingTimeElement.textContent = finalText;
+                    const targetElement = document.querySelector('${parentElementSelector}');
+                    if (targetElement) {
+                        targetElement.insertAdjacentElement('afterend', readingTimeElement);
+                    }else {
+                        console.error("Reading Time plugin. Error: element not found. Locator: '${parentElementSelector}'");
+                    }
+                }
                 calculateReadingTime();
             }            
         });
